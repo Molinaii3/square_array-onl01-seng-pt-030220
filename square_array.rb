e2d9de310b667ae3fa1ff338d67d7bbe4afb51e2
Michael Molina <l.13molina@gmail.com>
2020-03-14 04:38:32 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,12 +2,14 @@ def square_array(array)
   # your code here
   
   numbers= [1,2,3]
-  square_array(numbers)
-  => [1,4,9]
+  expect(numbers).to_not receive(:collect)
+    expect(numbers).to_not receive(:map)
+    expect(numbers).to_not receive(:inject)
+    square_array(numbers)
+  square_array(numbers) => [1,4,9]
   
   
   new_numbers=[9,10,16,25]
-  square_array(new_numbers)
-  => [89,100,256,625]
+  square_array(new_numbers) => [89,100,256,625]
   
 end
