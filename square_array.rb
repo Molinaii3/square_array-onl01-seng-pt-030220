4341a7f03624c8d40fc671c3a671351fe6551b58
Michael Molina <l.13molina@gmail.com>
2020-03-14 04:33:23 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,13 @@
 def square_array(array)
   # your code here
+  
   numbers= [1,2,3]
   square_array(numbers)
+  => [1,4,9]
   
-  new_numbers=[1,4,9]
+  
+  new_numbers=[9,10,16,25]
+  square_array(new_numbers)
+  => [89,100,256,625]
+  
 end
